ac102024349a1357727f03af342547940f6b0496
Laura Moreno <chipifsdev@gmail.com>
2025-03-23 17:54:58 +0100

    creado el servivio de new-post // he empezado a poner los validadores en el ts y en el html pero hay que continuarlo // tambien tengo la funcion checkError en el ts // he cambiado los botones del header y la posicion del footer // le he dado más estilos al formulario, sobretodo al boton de submit

--- blog_viajes/src/app/Pages/new-post/new-post.component.ts
+++ blog_viajes/src/app/Pages/new-post/new-post.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-post',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './new-post.component.html',
   styleUrl: './new-post.component.css'
 })
 export class NewPostComponent {
 
+  formulario: FormGroup = new FormGroup({
+    titulo: new FormControl('', [
+      Validators.required
+    ]),
+    texto: new FormControl('', [
+      Validators.required,
+      Validators.minLength(50)
+    ]),
+    imagen: new FormControl('', [
+      Validators.required
+    ]),
+    categoria: new FormControl('', [
+      Validators.required
+    ]),
+    fecha_aventura: new FormControl('', [
+      Validators.required
+    ])
+  })
+
+
+  onSubmit() {
+    throw new Error('Method not implemented.');
+  }
+
+  checkError(field: string, validator: string): boolean | undefined {
+    return this.formulario.get(field)?.hasError(validator) && this.formulario.get(field)?.touched
+  }
 }
