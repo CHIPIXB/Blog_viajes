d264a8921809e8c0177bd40f6dacea4f9012ee13
Laura Moreno <chipifsdev@gmail.com>
2025-03-23 15:20:15 +0100

    creado el gitignore de la carpeta assest images para meter ahi las imagenes que necesite // creado el componente new-post // añadido a rutas // estoy con el formulario, seguir por ahi

--- blog_viajes/src/app/app.routes.ts
+++ blog_viajes/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { NewPostComponent } from './Pages/new-post/new-post.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", pathMatch: 'full', redirectTo: 'normas' },
+    { path: "newpost", component: NewPostComponent },
+    { path: "**", redirectTo: 'normas' }
+];
