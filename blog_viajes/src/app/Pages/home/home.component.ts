df59e544552ddc7daf0f93bc18d38a335d1aafc4
Laura Moreno <chipifsdev@gmail.com>
2025-03-24 23:56:05 +0100

    He añadido la ruta home // he cambiado el fondo del header a madera y lo he fijado para que baje con el scroll vertical // html y css del home maquetados para que se vean todos los posts // inyectado el servicio en el home y añadido el metodo getAllPost //

--- blog_viajes/src/app/Pages/home/home.component.ts
+++ blog_viajes/src/app/Pages/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { NewPostService } from '../../Services/post.service';
+import { Post } from '../../Interfaces/post';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +10,8 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  private newPostService = inject(NewPostService)
 
+  arrPosts: Post[] = this.newPostService.getAllPost()
 
 }
--- blog_viajes/src/app/app.routes.ts
+++ blog_viajes/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { NewPostComponent } from './Pages/new-post/new-post.component';
+import { HomeComponent } from './Pages/home/home.component';
 
 export const routes: Routes = [
     { path: "", pathMatch: 'full', redirectTo: 'normas' },
     { path: "newpost", component: NewPostComponent },
+    { path: "home", component: HomeComponent },
     { path: "**", redirectTo: 'normas' }
 ];
