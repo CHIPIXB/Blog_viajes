40fb3742be8198d46ce0053fbbb67b55e0da437c
Laura Moreno <chipifsdev@gmail.com>
2025-03-25 17:58:26 +0100

    He creado el componente consejos, tengo que empezar a maquetarlo // he modificado unos margenes en la home porque se veian descuadrados

---
+++ blog_viajes/src/app/Pages/consejos/consejos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consejos',
+  imports: [],
+  templateUrl: './consejos.component.html',
+  styleUrl: './consejos.component.css'
+})
+export class ConsejosComponent {
+
+}
