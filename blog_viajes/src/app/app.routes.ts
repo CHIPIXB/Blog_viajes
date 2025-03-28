b659a46ef5d0b9ca6610b2a0ba2729c65d2aad22
Laura Moreno <chipifsdev@gmail.com>
2025-03-28 19:20:29 +0100

    Creado el componente search para el filtrado de posts // le he cambiado el nombre en el header de buscar a search para que esté todo el header en el mismo idioma // le he importado el router link al header para que vaya a la pagina search // he añadido al fichero de routes.ts la nueva ruta de search

--- blog_viajes/src/app/app.routes.ts
+++ blog_viajes/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { NewPostComponent } from './Pages/new-post/new-post.component';
 import { HomeComponent } from './Pages/home/home.component';
 import { PostComponent } from './Pages/post/post.component';
+import { SearchComponent } from './Pages/search/search.component';
 
 export const routes: Routes = [
     { path: "", pathMatch: 'full', redirectTo: 'home' },
     { path: "post/:idpost", component: PostComponent },
     { path: "newpost", component: NewPostComponent },
     { path: "home", component: HomeComponent },
+    { path: "search", component: SearchComponent },
     { path: "**", redirectTo: 'normas' }
 ];
